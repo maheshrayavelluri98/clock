38232672e8ea32bd38e22598a27fab95427970e0
maheshrayavelluri98 <maheshrayavelluri1717@gmail.com>
2024-12-31 17:33:07 +0530

    Digital Clock

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { DigitalClock } from "./components/digitalclock";
+
+function App() {
+  return (
+    <div className="App">
+      <DigitalClock />
+    </div>
+  );
+}
+
+export default App;
